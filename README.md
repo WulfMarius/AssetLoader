cb962d5613446caa3f2d8d8d6b9383c80e32d2df
WulfMarius <WulfMarius@users.noreply.github.com>
2017-08-14 17:54:37 +0200

    - added sample asset bundles - added script for Unity Editor to create asset bundles

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ This is a mod for **The Long Dark** by Hinterland Games Studio, Inc.
 It allows loading assets from external asset bundles.
 
 
-Currently only adds two console commands "chair" and "binoculars" for spawning test assets.
+- Currently only adds two console commands "chair" and "binoculars" for spawning test assets.
+- resources/binoculars and resources/leather_chair need to be copied to the "mods" folder
+- CreateAssetBundle.cs is a script for the Unity Editor to create own asset bundles; it need to be copied into the "Assets/Editor" folder of the Unity project
+
 
 
 Requires the [Mod Loader](https://github.com/zeobviouslyfakeacc/ModLoaderInstaller) to run.
---
+++ resources/CreateAssetBundle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateAssetBundle : MonoBehaviour
+{
+
+    [MenuItem("Assets/Build AssetBundle")]
+    static void ExportResource()
+    {
+        //string path = "Assets/AssetBundle/ExampleObject.unity3d";
+        Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        foreach (Object eachObject in selection)
+        {
+            if (eachObject is GameObject)
+            {
+                Debug.Log(eachObject);
+
+                GameObject gameObject = (GameObject)eachObject;
+
+                AssetBundleBuild build = new AssetBundleBuild();
+                build.assetBundleName = gameObject.name;
+
+                string path = AssetDatabase.GetAssetPath(eachObject);
+                build.assetNames = new string[] { path };
+
+                BuildPipeline.BuildAssetBundles("Assets/AssetBundles",
+                    new AssetBundleBuild[] { build },
+                    BuildAssetBundleOptions.UncompressedAssetBundle | BuildAssetBundleOptions.ForceRebuildAssetBundle,
+                    BuildTarget.StandaloneWindows);
+            }
+        }
+
+
+        //BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path,
+        //                               BuildAssetBundleOptions.CollectDependencies
+        //                             | BuildAssetBundleOptions.CompleteAssets, BuildTarget.Android);
+    }
+}
